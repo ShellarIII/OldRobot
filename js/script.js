ac23d71cbaffdabb6f38a33c56729821106d7ff8
ShellarIII <34601279+ShellarIII@users.noreply.github.com>
2018-02-13 20:47:18 +0400

    Scroll Progress Indicator, slider

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ $(function() {
 		$('.search-icon').toggleClass('search-icon-active');
 		$('.main-menu .logo').toggleClass('logo-active');
 	});
+	$(window).on('scroll', function(){
+	    var ratio = $(document).scrollTop()/(($(document).height() - $(window).height()) / 100);
+	    $(".progress_bar").width(ratio+"%");
+	});
+	var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      freeMode: true,
+			autoHeight: true,
+			centeredSlides: true
+    });
 });
